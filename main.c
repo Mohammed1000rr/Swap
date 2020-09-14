1f1d584e460c5ffbea8ecc19d7e1d69d4ac09356
Mohammed1000rr <mohammed.abdallah1000rr@gmail.com>
2020-09-14 14:07:03 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int , int);
+
+int main()
+{
+   int x , y;
+   printf("Please Enter x:");
+   scanf("%d" , &x);
+   printf("Please Enter y:");
+   scanf("%d" , &y);
+
+   swap(x,y);
+
+
+    return 0;
+}
+
+void swap(int x,int y)
+{
+
+    x = x+y;
+    y = x-y;
+    x = x-y;
+
+    printf("x = %d , y = %d ",x,y);
+
+}
